9f7826ee344a69400e25163069c091af54db6f67
Fernando N <me@fernandonunez.io>
2025-01-06 21:22:14 +0000

    Update fetching from mocks to real data.

--- internal/pkgdata/fetch.go
+++ internal/pkgdata/fetch.go
@@ -1,27 +1,56 @@
 package pkgdata
 
 import (
+	"bufio"
+	"fmt"
+	"os/exec"
+	"strings"
 	"time"
 )
 
-// mocks, for now
 func FetchPackages() ([]PackageInfo, error) {
-	packages := []PackageInfo{
-		{
-			Timestamp: time.Now().Add(-time.Hour * 24 * 3), // 3 days ago
-			Name:      "foo",
-			Reason:    "explicit",
-		},
-		{
-			Timestamp: time.Now().Add(-time.Hour * 24), // 1 day ago
-			Name:      "bar",
-			Reason:    "dependency",
-		},
-		{
-			Timestamp: time.Now(), // now
-			Name:      "baz",
-			Reason:    "explicit",
-		},
+	// expac queries Arch Linux + Arch-based package DBs
+	cmd := exec.Command("expac", "--timefmt=%Y-%m-%d %T", "%l\t%n\t%w")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, fmt.Errorf("error starting command: %w", err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		return nil, fmt.Errorf("error starting command: %w", err)
+	}
+
+	var packages []PackageInfo
+	scanner := bufio.NewScanner(stdout)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fields := strings.Split(line, "\t")
+
+		// check for correct field format, skip if not
+		if len(fields) != 3 {
+			continue
+		}
+
+		timestampStr, name, reason := fields[0], fields[1], fields[2]
+		timestamp, err := time.Parse("2006-01-02 15:04:05", timestampStr)
+		if err != nil {
+			continue
+		}
+
+		packages = append(packages, PackageInfo{
+			Timestamp: timestamp,
+			Name:      name,
+			Reason:    reason,
+		})
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error reading command output: %w", err)
+	}
+
+	if err := cmd.Wait(); err != nil {
+		return nil, fmt.Errorf("comand exited with error: %w", err)
 	}
 
 	return packages, nil
