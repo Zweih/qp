fbdb513033469f1a4159df61ca4bbd4e383533a7
Fernando N <me@fernandonunez.io>
2025-03-05 15:59:26 +0000

    Moved columns logic into render_json and render_table.

--- internal/display/render_json.go
+++ internal/display/render_json.go
@@ -3,13 +3,14 @@ package display
 import (
 	"encoding/json"
 	"fmt"
+	"yaylog/internal/consts"
 	"yaylog/internal/pkgdata"
 )
 
 func (o *OutputManager) renderJson(pkgs []pkgdata.PackageInfo, columnNames []string) {
 	filteredPackages := make([]pkgdata.PackageInfoJson, len(pkgs))
 	for i, pkg := range pkgs {
-		filteredPackages[i] = GetColumnJsonValues(pkg, columnNames)
+		filteredPackages[i] = getJsonValues(pkg, columnNames)
 	}
 
 	jsonOutput, err := json.MarshalIndent(filteredPackages, "", "  ")
@@ -18,4 +19,31 @@ func (o *OutputManager) renderJson(pkgs []pkgdata.PackageInfo, columnNames []str
 	}
 
 	o.writeLine(string(jsonOutput))
+}
+
+func getJsonValues(pkg pkgdata.PackageInfo, columnNames []string) pkgdata.PackageInfoJson {
+	filteredPackage := pkgdata.PackageInfoJson{}
+
+	for _, columnName := range columnNames {
+		switch columnName {
+		case consts.Date:
+			filteredPackage.Timestamp = &pkg.Timestamp
+		case consts.Name:
+			filteredPackage.Name = pkg.Name
+		case consts.Reason:
+			filteredPackage.Reason = pkg.Reason
+		case consts.Size:
+			filteredPackage.Size = pkg.Size // return in bytes for json
+		case consts.Version:
+			filteredPackage.Version = pkg.Version
+		case consts.Depends:
+			filteredPackage.Depends = pkg.Depends
+		case consts.RequiredBy:
+			filteredPackage.RequiredBy = pkg.RequiredBy
+		case consts.Provides:
+			filteredPackage.Provides = pkg.Provides
+		}
+	}
+
+	return filteredPackage
 }
--- internal/display/render_table.go
+++ internal/display/render_table.go
@@ -9,6 +9,21 @@ import (
 	"yaylog/internal/pkgdata"
 )
 
+type tableContext struct {
+	DateFormat string
+}
+
+var columnHeaders = map[string]string{
+	consts.Date:       "DATE",
+	consts.Name:       "NAME",
+	consts.Reason:     "REASON",
+	consts.Size:       "SIZE",
+	consts.Version:    "VERSION",
+	consts.Depends:    "DEPENDS",
+	consts.RequiredBy: "REQUIRED BY",
+	consts.Provides:   "PROVIDES",
+}
+
 // displays data in tab format
 func (o *OutputManager) renderTable(
 	pkgs []pkgdata.PackageInfo,
@@ -23,7 +38,7 @@ func (o *OutputManager) renderTable(
 		dateFormat = consts.DateTimeFormat
 	}
 
-	ctx := displayContext{DateFormat: dateFormat}
+	ctx := tableContext{DateFormat: dateFormat}
 
 	var buffer bytes.Buffer
 	w := tabwriter.NewWriter(&buffer, 0, 8, 2, ' ', 0)
@@ -49,11 +64,59 @@ func renderHeaders(w *tabwriter.Writer, columnNames []string) {
 	fmt.Fprintln(w, strings.Join(headers, "\t"))
 }
 
-func renderRows(w *tabwriter.Writer, pkg pkgdata.PackageInfo, columnNames []string, ctx displayContext) {
+func renderRows(w *tabwriter.Writer, pkg pkgdata.PackageInfo, columnNames []string, ctx tableContext) {
 	row := make([]string, len(columnNames))
 	for i, columnName := range columnNames {
-		row[i] = GetColumnTableValue(pkg, columnName, ctx)
+		row[i] = getTableValue(pkg, columnName, ctx)
 	}
 
 	fmt.Fprintln(w, strings.Join(row, "\t"))
+}
+
+func getTableValue(pkg pkgdata.PackageInfo, columnName string, ctx tableContext) string {
+	switch columnName {
+	case consts.Date:
+		return formatDate(pkg, ctx)
+	case consts.Name:
+		return pkg.Name
+	case consts.Reason:
+		return pkg.Reason
+	case consts.Size:
+		return formatSize(pkg.Size)
+	case consts.Version:
+		return pkg.Version
+	case consts.Depends:
+		return formatPackageList(pkg.Depends)
+	case consts.RequiredBy:
+		return formatPackageList(pkg.RequiredBy)
+	case consts.Provides:
+		return formatPackageList(pkg.Provides)
+	default:
+		return ""
+	}
+}
+
+// use time as parameter
+func formatDate(pkg pkgdata.PackageInfo, ctx tableContext) string {
+	return pkg.Timestamp.Format(ctx.DateFormat)
+}
+
+func formatPackageList(packages []string) string {
+	if len(packages) == 0 {
+		return "-"
+	}
+	return strings.Join(packages, ", ")
+}
+
+func formatSize(size int64) string {
+	switch {
+	case size >= consts.GB:
+		return fmt.Sprintf("%.2f GB", float64(size)/(consts.GB))
+	case size >= consts.MB:
+		return fmt.Sprintf("%.2f MB", float64(size)/(consts.MB))
+	case size >= consts.KB:
+		return fmt.Sprintf("%.2f KB", float64(size)/(consts.KB))
+	default:
+		return fmt.Sprintf("%d B", size)
+	}
 }
