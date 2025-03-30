aeefe915ce744421320b7a14f16c73d3f0ea74ba
Fernando N <me@fernandonunez.io>
2025-03-30 20:22:12 +0000

    Fix config output.

--- internal/config/config.go
+++ internal/config/config.go
@@ -42,7 +42,6 @@ type CliConfigProvider struct{}
 func (c *CliConfigProvider) GetConfig() (Config, error) {
 	cfg, err := ParseFlags(os.Args[1:])
 	if err != nil {
-		pflag.PrintDefaults()
 		return Config{}, err
 	}
 
@@ -82,7 +81,7 @@ func ParseFlags(args []string) (Config, error) {
 	pflag.BoolVarP(&allPackages, "all", "a", false, "Show all packages (ignores -l)")
 
 	pflag.StringArrayVarP(&filterInputs, "where", "w", []string{}, "Apply multiple filters (e.g. --where size=2KB:3KB --w name=vim)")
-	pflag.StringVarP(&sortInput, "order", "O", "date", "Order results by a field")
+	pflag.StringVarP(&sortInput, "order", "O", "date", "Order results by field")
 
 	pflag.BoolVarP(&hasNoHeaders, "no-headers", "", false, "Hide headers for table ouput (useful for scripts/automation)")
 	pflag.BoolVarP(&hasAllFields, "select-all", "A", false, "Display all available fields")
