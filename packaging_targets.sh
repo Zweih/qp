159975d8495513cf03bd3977fe253d6ec082c122
Fernando N <me@fernandonunez.io>
2025-05-08 16:53:31 +0000

    Add packaging targets.

---
+++ packaging_targets.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+deb_archs=(x86_64 armv7h aarch64 mipsle)
+aur_archs=(x86_64 armv7h aarch64)
+opkg_archs=(x86_64 armv7h aarch64 mipsle)
+
+should_package() {
+  local target="$1"
+  local arch="$2"
+  local -n list="$target"
+
+  for a in "${list[@]}"; do
+    [[ "$a" == "$arch" ]] && return 0
+  done
+
+  return 1
+}
