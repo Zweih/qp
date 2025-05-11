2edf6d2a27d1e5cfb4c8e24d6a53baaae4c070b8
Fernando N <me@fernandonunez.io>
2025-05-11 23:01:35 +0000

    Fix .deb installed-size.

--- package_deb.sh
+++ package_deb.sh
@@ -31,6 +31,12 @@ mkdir -p "$PKGDIR/DEBIAN"
 mkdir -p "$PKGDIR/usr/bin"
 mkdir -p "$PKGDIR/usr/share/doc/qp"
 mkdir -p "$PKGDIR/usr/share/man/man1"
+
+install -m 755 "$BIN" "$PKGDIR/usr/bin/qp"
+install -m 644 "$MANPAGE_PATH" "$PKGDIR/usr/share/man/man1/qp.1"
+install -m 644 "$NEWS_PATH" "$PKGDIR/usr/share/doc/qp/NEWS"
+install -m 644 "$COPYRIGHT_PATH" "$PKGDIR/usr/share/doc/qp/copyright"
+
 SIZE_KB=$(du -sk --exclude=DEBIAN "$PKGDIR" | cut -f1)
 
 cat >"$PKGDIR/DEBIAN/control" <<EOF
@@ -44,11 +50,6 @@ Maintainer: Fernando Nunez <me@fernandonunez.io>
 Description: qp - Query Packages. A CLI tool for querying installed packages.
 Homepage: https://github.com/Zweih/qp
 EOF
-
-install -m 755 "$BIN" "$PKGDIR/usr/bin/qp"
-install -m 644 "$MANPAGE_PATH" "$PKGDIR/usr/share/man/man1/qp.1"
-install -m 644 "$NEWS_PATH" "$PKGDIR/usr/share/doc/qp/NEWS"
-install -m 644 "$COPYRIGHT_PATH" "$PKGDIR/usr/share/doc/qp/copyright"
 
 dpkg-deb --build "$PKGDIR"
 mv "$PKGDIR.deb" "$OUTDIR/qp-v${VERSION}-${DEBARCH}.deb"
