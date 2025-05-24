f87a999f5571d3822f95beeb454d37721d17d5ac
Fernando N <me@fernandonunez.io>
2025-05-24 01:11:45 +0000

    Update interface.

--- api/driver/interface.go
+++ api/driver/interface.go
@@ -9,6 +9,5 @@ type Driver interface {
 	ResolveDeps(pkgs []*pkgdata.PkgInfo) ([]*pkgdata.PkgInfo, error)
 	LoadCache(cacheRoot string) ([]*pkgdata.PkgInfo, error)
 	SaveCache(cacheRoot string, pkgs []*pkgdata.PkgInfo) error
-	SourceModified() (int64, error)
-	IsCacheStale(cacheModTime int64) (bool, error)
+	IsCacheStale(cacheMtime int64) (bool, error)
 }
