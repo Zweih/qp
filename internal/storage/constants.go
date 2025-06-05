43869e2c80f6665cc47aaf7c5e85c98c45f4667b
Fernando N <me@fernandonunez.io>
2025-06-05 19:08:00 +0000

    Refactor: Regenerate history protobuf
    Regen history.

--- internal/storage/constants.go
+++ internal/storage/constants.go
@@ -2,7 +2,7 @@ package storage
 
 const (
 	cacheVersion   = 24 // bump when updating structure of PkgInfo/Relation/pkginfo.proto OR when dependency resolution is updated
-	historyVersion = 2
+	historyVersion = 3
 
 	xdgCacheHomeEnv = "XDG_CACHE_HOME"
 	homeEnv         = "HOME"
