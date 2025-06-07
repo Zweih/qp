3fb7a4fa95261a775879c506a0e4e4c5f5a8f910
Fernando N <me@fernandonunez.io>
2025-06-07 01:00:28 +0000

    Bump cache version.

--- internal/storage/constants.go
+++ internal/storage/constants.go
@@ -1,7 +1,7 @@
 package storage
 
 const (
-	cacheVersion   = 24 // bump when updating structure of PkgInfo/Relation/pkginfo.proto OR when dependency resolution is updated
+	cacheVersion   = 25 // bump when updating structure of PkgInfo/Relation/pkginfo.proto OR when dependency resolution is updated
 	historyVersion = 3
 
 	xdgCacheHomeEnv = "XDG_CACHE_HOME"
