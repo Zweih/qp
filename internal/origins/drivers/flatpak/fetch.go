2ccc109829fd82384375c7c5cb68534025f2c33f
Fernando N <me@fernandonunez.io>
2025-06-11 00:28:58 +0000

    WIP #1

--- internal/origins/drivers/flatpak/fetch.go
+++ internal/origins/drivers/flatpak/fetch.go
@@ -1,8 +1,13 @@
 package flatpak
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"qp/internal/origins/worker"
 	"qp/internal/pkgdata"
+	"strings"
+	"sync"
 )
 
 func fetchPackages(origin string, installDirs []string) ([]*pkgdata.PkgInfo, error) {
@@ -11,9 +16,76 @@ func fetchPackages(origin string, installDirs []string) ([]*pkgdata.PkgInfo, err
 		return []*pkgdata.PkgInfo{}, err
 	}
 
-	for _, pkgRef := range pkgRefs {
-		fmt.Println(*pkgRef)
+	if len(pkgRefs) < 1 {
+		return []*pkgdata.PkgInfo{}, nil
 	}
 
+	inputChan := make(chan *PkgRef, len(pkgRefs))
+	errChan := make(chan error, worker.DefaultBufferSize)
+	var errGroup sync.WaitGroup
+
+	for _, pkgRef := range pkgRefs {
+		inputChan <- pkgRef
+	}
+	close(inputChan)
+
 	return []*pkgdata.PkgInfo{}, nil
+}
+
+func parseMetadata(pkgRef *PkgRef) (*pkgdata.PkgInfo, error) {
+	file, err := os.Open(pkgRef.MetadataPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open %s metadta file %s: %w", pkgRef.Name, pkgRef.MetadataPath, err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var currentSection string
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+
+		if line == "" || strings.HasPrefix("#", line) {
+			continue
+		}
+
+		if strings.HasPrefix(line, "[") && strings.HasSuffix(line, "]") {
+			currentSection = strings.Trim(line, "[]")
+			continue
+		}
+
+		if strings.Contains(line, "=") {
+			key, value := parseKeyValue(line)
+		}
+	}
+}
+
+func parseKeyValue(line string) (string, string) {
+	parts := strings.SplitN(line, "=", 2)
+	if len(parts) != 2 {
+		return "", ""
+	}
+
+	return strings.TrimSpace(parts[0]), strings.TrimSpace(parts[1])
+}
+
+func applyMetadataField(
+	pkg *pkgdata.PkgInfo,
+	section string,
+	key string,
+	value string,
+) error {
+	switch section {
+	case sectionApplication:
+		return applyApplicationField
+	default:
+		// TODO: extension
+		return nil
+	}
+}
+
+func applyApplicationField(pkg *pkgdata.PkgInfo, key string, value string) error {
+  switch key
+
 }
