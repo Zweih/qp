52bd872bf7349fe719ce9bbe945b8a49d2dbe000
Fernando N <me@fernandonunez.io>
2025-07-22 22:38:09 +0000

    Feature: Additional date query options (AM/PM, Timezone, Seconds)

--- internal/consts/dates.go
+++ internal/consts/dates.go
@@ -1,6 +1,15 @@
 package consts
 
 const (
-	DateOnlyFormat = "2006-01-02"
-	DateTimeFormat = "2006-01-02 03:04:05 PM MST"
+	DateOnlyFormat             = "2006-01-02"
+	DateTimeMinuteFormat       = "2006-01-02 15:04"
+	DateTimeSecondFormat       = "2006-01-02 15:04:05"
+	DateTime12HourMinuteFormat = "2006-01-02 3:04 PM"
+	DateTime12HourSecondFormat = "2006-01-02 3:04:05 PM"
+
+	DateOnlyTzFormat             = "2006-01-02 MST"
+	DateTimeMinuteTzFormat       = "2006-01-02 15:04 MST"
+	DateTimeSecondTzFormat       = "2006-01-02 15:04:05 MST"
+	DateTime12HourMinuteTzFormat = "2006-01-02 3:04 PM MST"
+	DateTime12HourSecondTzFormat = "2006-01-02 03:04:05 PM MST"
 )
