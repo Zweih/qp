2fe134f5adfe8fab40c1a615873400ac88f87feb
Fernando N <me@fernandonunez.io>
2025-08-13 19:10:01 +0000

    Add caching.

--- internal/origins/drivers/apk/driver.go
+++ internal/origins/drivers/apk/driver.go
@@ -3,7 +3,9 @@ package apk
 import (
 	"os"
 	"qp/internal/consts"
+	"qp/internal/origins/shared"
 	"qp/internal/pkgdata"
+	"qp/internal/storage"
 )
 
 type ApkDriver struct{}
@@ -26,13 +28,27 @@ func (d *ApkDriver) ResolveDeps(pkgs []*pkgdata.PkgInfo) ([]*pkgdata.PkgInfo, er
 }
 
 func (d *ApkDriver) LoadCache(cacheRoot string) ([]*pkgdata.PkgInfo, error) {
-	return []*pkgdata.PkgInfo{}, nil
+	return storage.LoadProtoCache(cacheRoot)
 }
 
 func (d *ApkDriver) SaveCache(cacheRoot string, pkgs []*pkgdata.PkgInfo) error {
-	return nil
+	return storage.SaveProtoCache(cacheRoot, pkgs)
 }
 
 func (d *ApkDriver) IsCacheStale(cacheMtime int64) (bool, error) {
-	return true, nil
+	isDbStale, err := shared.IsDirStale(d.Name(), apkDbPath, cacheMtime)
+	if err != nil {
+		return false, err
+	}
+
+	if isDbStale {
+		return true, nil
+	}
+
+	isWorldStale, err := shared.IsDirStale(d.Name(), apkWorldPath, cacheMtime)
+	if err != nil {
+		return false, nil
+	}
+
+	return isWorldStale, nil
 }
